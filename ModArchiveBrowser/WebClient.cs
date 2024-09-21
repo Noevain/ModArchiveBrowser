d4f06ed0be685ffc9463d1e0fc09b5b50e615077
Noevain <noevain@gmail.com>
2024-09-21 18:02:51 +0200

    Updated Xpath parsing

--- ModArchiveBrowser/WebClient.cs
+++ ModArchiveBrowser/WebClient.cs
@@ -54,68 +54,29 @@ namespace ModArchiveBrowser
 
         public static List<ModThumb> ParseHomePage(HtmlDocument homepage)
         {
-            var modLists = homepage.DocumentNode.SelectNodes("//ul[contains(@class, 'glide__slides')]");
             List<ModThumb> modthumbnails = new List<ModThumb>();
-            foreach (var modList in modLists)
+            HtmlNodeCollection titleNodes = homepage.DocumentNode.SelectNodes("//div[contains(@class, 'card-body')]//h5[contains(@class, 'card-title')]");
+            HtmlNodeCollection urlNodes = homepage.DocumentNode.SelectNodes("//a[contains(@href, '/modid/')]//@href");
+            HtmlNodeCollection thumbUrlNodes = homepage.DocumentNode.SelectNodes("//div[contains(@class, 'mod-card-img-container')]//img[contains(@class, 'card-img-top mod-card-img')]/@src");
+            HtmlNodeCollection authorNameNodes = homepage.DocumentNode.SelectNodes("//div[contains(@class, 'card-body')]//p[contains(@class, 'card-text')]//a");
+            HtmlNodeCollection authorUrlNodes = homepage.DocumentNode.SelectNodes("//div[contains(@class, 'card-body')]//p[contains(@class, 'card-text')]//a/@href");
+            HtmlNodeCollection typeNodes = homepage.DocumentNode.SelectNodes("//div[contains(@class, 'card-body')]//p[contains(@class, 'card-text')]//code[contains(text(), 'Type')]");
+            HtmlNodeCollection gendersNodes = homepage.DocumentNode.SelectNodes("//div[contains(@class, 'card-body')]//p[contains(@class, 'card-text')]//code[contains(text(), 'Genders')]");
+
+            int size = titleNodes.Count;
+
+            for (int i = 0; i < size; i++)
             {
-                Plugin.Logger.Debug("Going through modList");
-                var mods = modList.SelectNodes(".//li");
-
-                foreach (var mod in mods)
-                {
-                    Plugin.Logger.Debug("Going through a mod");
-                    string name = string.Empty;
-                    string url = string.Empty;
-                    string url_thumb = string.Empty;
-                    string author_url = string.Empty;
-                    string author = string.Empty;
-                    // mod title
-                    var h5node = mod.SelectSingleNode(".//div[1]//h5");
-                    if (h5node != null)
-                    {
-                        name = h5node.InnerText;
-                        Console.WriteLine("H5 Text: " + name);
-                    }
-                    else//caught an item that is not a mod?Didnt find one in the raw html but there are empty entries so w/e
-                    {
-                        break;
-                    }
-
-                    // modurl and thumbnail url
-                    var anchorNode = mod.SelectSingleNode(".//div[2]//a[starts-with(@href, '/modid/')]");
-                    if (anchorNode != null)
-                    {
-                        url = anchorNode.GetAttributeValue("href", string.Empty);
-                        Console.WriteLine("Filtered Href: " + url);
-
-                        var imgNode = anchorNode.SelectSingleNode(".//img");
-                        if (imgNode != null)
-                        {
-                            url_thumb = imgNode.GetAttributeValue("src", string.Empty);
-                            Console.WriteLine("Nested img src: " + url_thumb);
-                        }
-                    }
-
-                    //Author infos
-                    var nestedAnchorInP = mod.SelectSingleNode(".//div[3]//p//a");
-                    if (nestedAnchorInP != null)
-                    {
-                        author_url = nestedAnchorInP.GetAttributeValue("href", string.Empty);
-                        author = nestedAnchorInP.InnerText;
-                    }
-
-                    // Categories
-                    /* var codeNodes = mod.SelectNodes(".//div[4]//p//code");
-                     if (codeNodes != null && codeNodes.Count >= 2)
-                     {
-                         Console.WriteLine("First <code> tag: " + codeNodes[0].InnerText);
-                         Console.WriteLine("Second <code> tag: " + codeNodes[1].InnerText);
-                     }*/
-                    Plugin.Logger.Debug("Going through modList");
-                    modthumbnails.Add(new ModThumb(name, url, author, url_thumb, author_url));
-                }
-
+                string title = titleNodes[i].InnerText;
+                string modUrl = urlNodes[i].GetAttributeValue("href", "none");
+                string thumbUrl = thumbUrlNodes[i].GetAttributeValue("src", "none");
+                string authorName = authorNameNodes[i].InnerText;
+                string authorUrl =  authorUrlNodes[i].GetAttributeValue("href", "none");
+                string type = typeNodes[i].InnerText;
+                string gender = gendersNodes[i].InnerText;
+                modthumbnails.Add(new ModThumb(title, modUrl, authorName, thumbUrl, authorUrl));
             }
+
             return modthumbnails;
         }
         public WebClient() {
