43537a7c04f0eda07453c6db76cc36bbd4b71fca
Noevain <noevain@gmail.com>
2024-09-26 17:33:45 +0200

    Make modWindow go to front on selection

--- ModArchiveBrowser/Windows/MainWindow.cs
+++ ModArchiveBrowser/Windows/MainWindow.cs
@@ -59,6 +59,7 @@ public class MainWindow : Window, IDisposable
                         {
                             plugin.modWindow.Toggle();
                         }
+                        plugin.modWindow.BringToFront();
                     }
                     catch(Exception e)
                     {
